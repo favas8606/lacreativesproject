147d122e1fe7e7d923090523ef38618a549f9c18
favas <89352083+favas8606@users.noreply.github.com>
2024-01-22 15:18:29 +0530

    Update views.py

--- lacreatives/views.py
+++ lacreatives/views.py
@@ -1,32 +1,30 @@
 
 # Create your views here.
 from django.shortcuts import render
-from . models import Message
-import time
 
 # Create your views here.
 def index(request):
 
-    try:
-        time.sleep(2)
+    # try:
+    #     time.sleep(2)
 
-        if request.method == 'POST':
-            name = request.POST['name']
-            phonenumber = request.POST['phonenumber']
-            email = request.POST['email']
-            message = request.POST['message']
+    #     if request.method == 'POST':
+    #         name = request.POST['name']
+    #         phonenumber = request.POST['phonenumber']
+    #         email = request.POST['email']
+    #         message = request.POST['message']
 
-            if len(phonenumber) < 10 or name is None or email is None or message is None :
-                 return render(request, 'index.html')
-            else:
-                register = Message(name=name, phonenumber=phonenumber,email=email,messages=message)
-                if register is not None:
-                    register.save()
+    #         if len(phonenumber) < 10 or name is None or email is None or message is None :
+    #              return render(request, 'index.html')
+    #         else:
+    #             register = Message(name=name, phonenumber=phonenumber,email=email,messages=message)
+    #             if register is not None:
+    #                 register.save()
 
            
 
-    except:
-        print('Error occured')
+    # except:
+    #     print('Error occured')
             
 
 
